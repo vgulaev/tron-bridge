25d24593122d062bfa752cf72f1cde37a06fd260
vgulaev <vgulaev@yandex.ru>
2023-10-16 22:24:51 +0500

    Up

--- app_ecdsa/src/lib.rs
+++ app_ecdsa/src/lib.rs
@@ -1,4 +1,52 @@
-pub fn hex_str_to_limbs(numb: &str) -> Vec<u64> {
+use std::ops::Mul;
+
+const GX: &str = "79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798";
+
+pub fn g() -> Point {
+  Point {
+    x: natural_from_hex(GX),
+    y: natural_from_hex("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8"),
+    z: malachite::Natural::from(1u32),
+  }
+}
+
+#[derive(Debug)]
+pub struct Point {
+  pub x: malachite::Natural,
+  pub y: malachite::Natural,
+  pub z: malachite::Natural,
+}
+
+// impl Point {
+//   pub fn g() -> Point {
+//     Point {
+//     }
+//   }
+// }
+
+impl Mul<malachite::Natural> for Point {
+  type Output = Self;
+  fn mul(self, other: malachite::Natural) -> Self::Output {
+    Self {
+      x: malachite::Natural::from(1u32),
+      y: malachite::Natural::from(1u32),
+      z: malachite::Natural::from(1u32),
+    }
+  }
+}
+
+impl Mul<u128> for Point {
+  type Output = Self;
+  fn mul(self, other: u128) -> Self::Output {
+    println!("Mul u128");
+    if 1 == other {
+      return self;
+    }
+    self * malachite::Natural::from(other)
+  }
+}
+
+pub fn natural_from_hex(numb: &str) -> malachite::Natural {
   let mut res: Vec<u64> = Vec::new();
   let mut word: String = "".to_owned();
   for (ind, char) in numb.split("").enumerate() {
@@ -8,5 +56,16 @@ pub fn hex_str_to_limbs(numb: &str) -> Vec<u64> {
       word = "".to_owned();
     }
   }
-  res
+  malachite::Natural::from_limbs_desc(&res)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn check_g_mul() {
+    let p = g() * 1;
+    assert_eq!(p.x, natural_from_hex(GX));
+  }
 }
--- bridge/src/sign.rs
+++ bridge/src/sign.rs
@@ -1,10 +1,26 @@
 use serde_json::{Value, Error};
-use app_ecdsa::hex_str_to_limbs;
+use rand::Rng;
+use malachite_base::num::random::random_primitive_ints;
+use malachite_base::random::Seed;
+use malachite_nz::natural::random::get_random_natural_with_bits;
+use app_ecdsa::{natural_from_hex, g};
+
+fn get_rnd_seed() -> Seed {
+  let mut bytes: [u8; 32] = [0; 32];
+  for i in 0..32 {
+    bytes[i] = rand::thread_rng().gen_range(0..255);
+  }
+  Seed::from_bytes(bytes)
+}
 
 fn sign_hex_number(hexed: &str) -> String {
   println!("hexed: {:?}", hexed);
-  let numb = malachite::Natural::from_limbs_desc(&hex_str_to_limbs(&hexed));
+  let numb = natural_from_hex(&hexed);
+  let k = get_random_natural_with_bits(&mut random_primitive_ints(get_rnd_seed()), 255);
+  let r = g() * k.clone();
+  println!("k: {:?}", k.to_string());
   println!("number: {:?}", numb.to_string());
+  println!("r: {:?}", r.x);
   String::from("fsdfsfdsfdsf")
 }
 
